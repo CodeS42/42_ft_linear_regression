d5f9112177b36a95e75168b10183b4897d1a6d02
Scode0 <gandumdiagne.serena@gmail.com>
2025-12-24 18:49:16 +0100

    add read_thetas() to set thetas to 0 when the model hasn't been trained yet

--- estimated_price.py
+++ estimated_price.py
@@ -1,14 +1,21 @@
+def read_thetas():
+    try:
+        file = open("thetas.txt", "r")
+        content = file.read()
+        thetas = content.split(sep="\n")
+        return thetas
+    except FileNotFoundError:
+        return [0, 0]
+
 def estimated_price(mileage, theta0, theta1):
     return float(theta0) + (float(theta1) * mileage)
 
 
 def main():
     try:
-        file = open("thetas.txt", "r")
-        content = file.read()
-        thetas = content.split(sep="\n")
         print("Provides mileage to estimate the price:")
         mileage = int(input())
+        thetas = read_thetas()
         print(estimated_price(mileage, thetas[0], thetas[1]))
     except Exception as e:
         print(f"Error: {e}")
